f8fa3c44a39e8497fb2c012d3f54ba8b3c035e82
Bruno Amaral <mail@brunoamaral.eu>
2024-11-21 14:51:20 +0000

    add items to safelist

--- themes/Hugo-Now-UI-Pro/assets/css/postcss.config.js
+++ themes/Hugo-Now-UI-Pro/assets/css/postcss.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-    plugins: {
-        "@fullhuman/postcss-purgecss": {
-            content: ["./content/**/*.html", "./content/**/*.md"],
-            safelist: {
-                greedy: ["/.animate.*/",
-                    "/.text-align.*/"
-                ]
-            },
-            // fontFace: false,
-            variables: false
-        },
-        autoprefixer: {},
-        cssnano: {
-            preset: [
-                "default",
-                { "discardComments": { "removeAll": true } }
-            ]
-        }
-
-        //cssnano: { preset: 'default' }
-    }
+	plugins: {
+		"@fullhuman/postcss-purgecss": {
+			content: ["./content/**/*.html", "./content/**/*.md", "/layouts/**/*.html"],
+			safelist: {
+				greedy: [
+					"/.animate.*/",
+					"/.text-align.*/",
+                    "/.clear-filter.*",
+					"\\[filter-color\\=.*\\]" // Matches any [filter-color="..."]
+				]
+			},
+			// fontFace: false,
+			variables: false
+		},
+		autoprefixer: {},
+		cssnano: {
+			preset: [
+				"default",
+				{ "discardComments": { "removeAll": true } }
+			]
+		},
+	}
 };
